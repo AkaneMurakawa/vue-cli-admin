42b2a092ece93f8728a68031154eccf452070f64
AkaneMurakawa <chenshjing@gmail.com>
2023-03-21 11:27:54 +0800

    修复两个滚动条问题，footer内容调整为垂直水平居中

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,11 +1,13 @@
 <template>
     <el-footer>
-        <span>版权&copy;&nbsp;2023&nbsp;</span>
-        <span v-show="new Date().getFullYear() != 2023">
-            - {{ new Date().getFullYear() }}
-        </span>
-        <a href="/">编程改变世界</a>
-        <a href="#">关于我们</a>
+        <div>
+            <span>版权&copy;&nbsp;2023&nbsp;</span>
+            <span v-show="new Date().getFullYear() != 2023">
+                - {{ new Date().getFullYear() }}
+            </span>
+            <a href="/">编程改变世界</a>
+            <a href="#">关于我们</a>
+        </div>
     </el-footer>
 </template>
 
