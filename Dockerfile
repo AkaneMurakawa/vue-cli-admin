efbbd8fdbf77631f02afeca410f5eac7201454d3
AkaneMurakawa <chenshjing@gmail.com>
2023-03-31 18:25:16 +0800

    去除重定向

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,8 @@ FROM nginx
 MAINTAINER github
 VOLUME /tmp
 ENV LANG en_US.UTF-8
-RUN echo "mkdir /var/www/html" \
-    &&  mkdir -p /var/www/html
 
-ADD dist/ /var/www/html/
+ADD dist/ /usr/share/nginx/html/
 ADD nginx.conf /etc/nginx/conf.d/default.conf
 EXPOSE 80
 EXPOSE 443
--- src/http/axios/index.js
+++ src/http/axios/index.js
@@ -77,7 +77,6 @@ requests.interceptors.response.use((response) => {
                 break;
             case 404:
                 error.message = '请求错误,未找到该资源'
-                window.location.href = "/NotFound"
                 break;
             case 405:
                 error.message = '请求方法未允许'
